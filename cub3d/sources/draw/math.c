3a5d389ee793358f057ba9d1f5f1dacf59eae1c4
mistery5677 <miguel.costa.5677@gmail.com>
2025-02-11 23:29:20 +0000

    Textures fixed

--- cub3d/sources/draw/math.c
+++ cub3d/sources/draw/math.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   math.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: miafonso <miafonso@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mistery576 <mistery576@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 15:23:59 by miafonso          #+#    #+#             */
-/*   Updated: 2025/02/10 17:13:00 by miafonso         ###   ########.fr       */
+/*   Updated: 2025/02/11 23:28:23 by mistery576       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,9 @@ int	verify_side(t_data *data, float ray_x, float ray_y)
 	wall_x /= BLOCK;
 	wall_x -= floor (wall_x);
 	tex_x = (int)(wall_x * (float)64);
+	if ((data->wall->side == VERTICAL && ray_x < data->player->x_pst)
+		|| (data->wall->side == HORIZONTAL && ray_y > data->player->y_pst))
+		tex_x = 64 - tex_x - 1;
 	if (tex_x < 0)
 		tex_x = 0;
 	if (tex_x >= 64)
